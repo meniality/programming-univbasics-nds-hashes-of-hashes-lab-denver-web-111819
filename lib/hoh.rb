d69502570ce5d84d09efa43889e6c4261f8d5e07
patrick hoehn <patrh_99@hotmail.com>
2019-11-09 00:50:37 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,7 +38,13 @@ def naming_system
       label: "Class",
       sub_category: {
         label: "Order",
-        
+        sub_category: {
+          label: "Family",
+          sub_category: {
+            label: "Genus",
+            
+          }
+        }
       }
     }
   }
